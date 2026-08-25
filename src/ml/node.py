27fe16f84c2253e16601a5b3a29f1c2f1c633038
Bartosz Kafel <bartosz.kafel@gmail.com>
2026-08-25 07:28:42 +0200

    Node class for further ML development

---
+++ src/ml/node.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self, feature=None, threshold=None,
+                 left=None, right=None, prediction=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.prediction = prediction
